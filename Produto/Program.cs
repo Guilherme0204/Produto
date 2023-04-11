0a9ae32665bf62449ce30e273122333520ac2a0c
Guilherme0204 <gui2605costa@gmail.com>
2023-04-11 20:11:47 -0300

    Adicionar arquivos de projeto.

---
+++ Produto/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Produto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conuole.WriteLine("Informe o produto ");
+            string produto = Console.ReadLine();
+            Console.WriteLine($"Informe o valor do {produto}" );
+            double valProduto = Convert.ToDouble(Console.ReadLine());
+            if (valProduto < 20)
+            {
+                double lucro = valProduto * 145 / 100;
+              //lucro = valProduto + lucro;
+                Console.WriteLine(lucro);
+            } else
+            {
+                double lucro = valProduto * 0.30;
+                lucro = valProduto + lucro;
+                Console.WriteLine(lucro);
+            }
+            Console.ReadKey();
+        }
+    }
+}
